d066942c0a979488066f525be0df3e1399911076
test <ahimovdaniil2@gmail.com>
2024-06-12 22:37:33 +0300

    init

---
+++ src/main/java/by/kovalski/alexsystem/AlexSystemApplication.java
@@ -0,0 +1,13 @@
+package by.kovalski.alexsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlexSystemApplication.class, args);
+  }
+
+}
---
+++ src/test/java/by/kovalski/alexsystem/AlexSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kovalski.alexsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
